e13e0810de29636081ea287d3bdf5a3cec52259c
xiaoxiaoweii <2679656468@qq.com>
2021-02-20 21:58:07 +0800

    devserver配置

--- webpack.config.js
+++ webpack.config.js
@@ -104,4 +104,17 @@ module.exports = {
   // 模式 开发环境 development/生产环境 production
   mode: "development",
   // mode: 'production'
+
+  // 开发服务器 devServer 用来自动化(自动编译 自动打开浏览器 自动刷新浏览器) 
+  // 特点: 只会在内存中编译打包 不会有任何输出
+  // 启动devServer指令为: npx webpack-dev-server
+  devServer:{
+    contentBase: resolve(__dirname,'build'),
+    // 启动gzip压缩
+    compress: true,
+    // 指定开发端口号
+    port: 3000,
+    // 自动打开浏览器
+    open: true
+  }
 };
